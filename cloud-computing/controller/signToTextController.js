4228d1d8893cad1aabb8a2738807dcb11e7e648d
AndikaAnanda <andikaananda63@gmail.com>
2023-12-20 21:34:29 +0700

    add save to cloud storage feature to sign-to-text handler

--- cloud-computing/controller/signToTextController.js
+++ cloud-computing/controller/signToTextController.js
@@ -1,13 +1,23 @@
 const { loadModel, predict } = require('../model/mlModel')
+const { Storage } = require('@google-cloud/storage')
 const fs = require('fs').promises
 const path = require('path')
 
+const storage = new Storage({
+    projectId: 'signlingo-app',
+    keyFilename: 'C:\\Users\\User\\OneDrive\\Desktop\\SignLingo-main\\cloud-computing\\serviceAccounts.json'
+})
+const bucketName = 'signling-photoshot-results'
 
 const signToText_post = async (req, res) => {
     const model = await loadModel()
     try {
         const imageBuffer = req.file.buffer
         const predictions = await predict(model, imageBuffer)
+
+        // save image to cloud storage
+        const filename = `image-${Date.now()}.jpg`
+        await saveToCloudStorage(bucketName, filename, imageBuffer)
         res.status(201).json({
             message: `Prediction result: ${predictions}`
         })
@@ -17,6 +27,28 @@ const signToText_post = async (req, res) => {
             message: 'Internal Server Error'
         })
     }
+}
+
+const saveToCloudStorage = async (bucketName, filename, fileBuffer) => {
+    const bucket = storage.bucket(bucketName);
+    const file = bucket.file(filename);
+    const stream = file.createWriteStream({
+        metadata: {
+            contentType: 'image/jpeg', // set the appropriate content type
+        },
+    });
+
+    return new Promise((resolve, reject) => {
+        stream.on('error', (error) => {
+            reject(error);
+        });
+
+        stream.on('finish', () => {
+            resolve();
+        });
+
+        stream.end(fileBuffer);
+    });
 }
 
 module.exports = signToText_post
